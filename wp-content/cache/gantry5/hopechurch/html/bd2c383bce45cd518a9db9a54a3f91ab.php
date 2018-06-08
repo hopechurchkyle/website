8476a7de7738475b3f233e6cbd134980b52fca43
root <root@hope-wordpress.c.i-return-206100.internal>
2018-06-08 23:00:22 +0000

    cleanup

--- wp-content/cache/gantry5/hopechurch/html/bd2c383bce45cd518a9db9a54a3f91ab.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
-    '_version' => 1,
-    'id' => '5b120bdc033310.86509219',
-    'content' => '<div id="custom-3190-particle" class="g-content g-particle">            <h1>Woot!</h1>
-            </div>'
-];
